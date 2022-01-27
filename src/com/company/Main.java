8523d2d301c1675782bf83a6cb886ef1148ffe0c
Kalyan20 <keenalakalyan@gmail.com>
2022-01-27 14:36:39 -0700

    second commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,22 +1,25 @@
 package com.company;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.OptionalDouble;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
 
     public static void main(String[] args) {
-
+        String input = "";
+        String fileName = "";
+        String fileLocation = "";
+        String equation = "";
+        String line = "";
         try( BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
 
-            String input = "";
-            String fileName = "";
-            String fileLocation = "";
-            String equation = "";
-            while (!"q".equalsIgnoreCase(input)) {
+
+            while (!"exit".equalsIgnoreCase(input)) {
 
                 System.out.print("Enter File Name : ");
                 fileName = br.readLine();
@@ -25,32 +28,138 @@ public class Main {
                 System.out.print("Enter File Location : ");
                 fileLocation = br.readLine();
                 System.out.println("Entered File Location : " + fileLocation);
-                //parsing a CSV file into Scanner class constructor
+
+                System.out.print("Enter compute equation : ");
+                equation = br.readLine();
+                System.out.println("Entered compute equation : " + equation);
+                //parsing a CSV file
                 File file = new File(fileLocation +"/" + fileName);
-                Scanner sc = new Scanner(file);
-                sc.useDelimiter(",");   //sets the delimiter pattern
-                while (sc.hasNext())  //returns a boolean value
-                {
-                    System.out.print(sc.next());  //find and returns the next complete token from this scanner
+                BufferedReader br1 = new BufferedReader(new FileReader(file));
+                List<Double> collectDeltaSquares = null;
+                if((line = br1.readLine()) != null) {
+                    collectDeltaSquares = new ArrayList<>();
                 }
-                sc.close();  //closes the scanner
-                System.out.print("Continue  : ");
+                while ((line = br1.readLine()) != null) {
+                    // use comma as separator
+                    String[] cols = line.split(",");
+                    if(cols[0].equalsIgnoreCase("x") || cols[1].equalsIgnoreCase("Y")) {
+                        continue;
+                    }
+                    int x = Integer.parseInt(cols[0]);
+                    int y = Integer.parseInt(cols[1]);
+
+                    float z = 0 ;
+                    String firstSplitEquation;
+                    String secondSplitEquation;
+                    String operation;
+                    // split equation string capture sepearated equations and operation
+                    String[] equationSplit = equation.split(" ");
+                    firstSplitEquation =  equationSplit[0];
+                    operation = equationSplit[1];
+                    secondSplitEquation = equationSplit[2];
+                    //compute split equations
+                    Float firstEquationComputed = compute(firstSplitEquation, x, y);
+                    Float secondEquationComputed = compute(secondSplitEquation, x, y);
+
+                    // compute operation
+                    if(operation.equals("+")) {
+                        z = firstEquationComputed + secondEquationComputed ;
+                    }
+                    if(operation.equals("-")) {
+                        z = firstEquationComputed - secondEquationComputed ;
+                    }
+
+
+                    System.out.println("Coulmn 1= " + cols[0] + " , Column 2=" + cols[1]);
+                    System.out.println("z= " + z);
+
+                    double square = Math.pow(z - (float) y, 2);
+                    System.out.println("square= " + square);
+                    if(collectDeltaSquares != null) {
+                        collectDeltaSquares.add(square);
+                    }
+                }
+
+                OptionalDouble average = collectDeltaSquares
+                        .stream()
+                        .mapToDouble(a -> a)
+                        .average();
+                double meanDeltaSquares = average.isPresent() ? average.getAsDouble() : 0;
+                System.out.println("Mean of Delta Squares:" +meanDeltaSquares );
+
+                System.out.print("Continue with another file : ");
                 input = br.readLine();
         }
 
-            System.out.println("bye bye!");
+            System.out.println("System terminated by the user");
 
-        } catch (IOException e) {
+        } catch (FileNotFoundException fn){
+            System.out.println("File not found :" +fileName + " in the location :" +fileLocation);
+        }
+        catch (IOException e) {
             e.printStackTrace();
-        } finally {
-           /* if (br != null) {
-                try {
-                    br.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }*/
         }
     }
-}	// write your code here
+
+    private static float compute(String splitEquation, int x, int y) {
+      float computedValue = 0;
+      boolean extractedNumbers = false;
+      boolean hasX = false;
+      boolean hasY = false;
+        if(splitEquation.contains("x")) {
+            List<Float> numbers = extractNumbers(splitEquation);
+            extractedNumbers = true;
+            hasX = true;
+            for( Float number: numbers) {
+                if(computedValue != 0) {
+                    computedValue = number * computedValue;
+                } else {
+                    computedValue = number;
+                }
+            }
+            if(computedValue != 0) {
+                computedValue = computedValue * x;
+            } else {
+                computedValue = x;
+            }
+        }
+
+        if(splitEquation.contains("y")) {
+            hasY = true;
+            if(extractedNumbers == false) {
+                List<Float> numbers = extractNumbers(splitEquation);
+                for (Float number : numbers) {
+
+                    if (computedValue != 0) {
+                        computedValue = number * computedValue;
+                    } else {
+                        computedValue = number;
+                    }
+                }
+            }
+            if(computedValue != 0) {
+                computedValue = computedValue * y;
+            } else {
+                computedValue = y;
+            }
+        }
+       if(hasX == false && hasY == false) {
+           computedValue = Float.parseFloat(splitEquation);
+       }
+
+       return computedValue;
+    }
+
+    private static List<Float> extractNumbers(String splitEquation) {
+        String strPattern = "-?\\d+(\\.\\d+)?";
+        Pattern p = Pattern.compile(strPattern);
+        Matcher m = p.matcher(splitEquation);
+        List<Float> listNumbers = new ArrayList<>();
+        while (m.find()) {
+            System.out.println(m.group());
+            listNumbers.add(Float.parseFloat(m.group()));
+        }
+        return listNumbers;
+    }
+}
 
